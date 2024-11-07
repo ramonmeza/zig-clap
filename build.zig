88b91fab4845d3493fa0e431ef0507bb635407cb
Ramon Meza <ramon_meza@live.com>
2024-11-07 16:55:53 -0500

    Make module

--- build.zig
+++ build.zig
@@ -1,21 +1,12 @@
 const std = @import("std");
 
-pub fn addClap(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *std.Build.Step.Compile {
-    const lib = b.addStaticLibrary(.{
-        .name = "zig-clap",
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    _ = b.addModule("clap", .{
         .root_source_file = b.path("src/clap.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    lib.linkLibC();
-
-    return lib;
-}
-
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-    const clap = addClap(b, target, optimize);
-    b.installArtifact(clap);
 }
